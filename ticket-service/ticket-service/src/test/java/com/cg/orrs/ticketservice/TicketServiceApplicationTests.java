6f6d3d59158c8547d7404d73b07d0ce1b37d9fa0
Y0unu5 <mkyounus541@gmail.com>
2022-07-12 11:54:06 +0530

    adding ticket service

---
+++ ticket-service/ticket-service/src/test/java/com/cg/orrs/ticketservice/TicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.orrs.ticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
